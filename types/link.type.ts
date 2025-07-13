f9f23c4577ab4802bc634936f93781560116cdb6
femmytedrey <femmytedrey@gmail.com>
2025-07-13 17:00:31 +0100

    fix social link typo

--- types/link.type.ts
+++ types/link.type.ts
@@ -1,6 +1,6 @@
 export enum Links {
   GITHUB = "https://github.com/femmytedrey",
   LINKEDIN = "https://www.linkedin.com/in/ayomide-adeyemo-61865526b/",
-  TWITTER = "https://twitter.com/femidev",
+  TWITTER = "https://x.com/femidevs",
   CV = "https://firebasestorage.googleapis.com/v0/b/my-portfolio-65091.appspot.com/o/Adeyemo_Ayomide_FullStack_Developer_CV.pdf?alt=media&token=ba00365f-ba2d-4cca-bc36-e95a59c93bce"
 }
