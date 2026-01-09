f3f27043c5fa3f2ae457d9f552bd6f0d0148dff8
femmytedrey <femmytedrey@gmail.com>
2026-01-10 05:26:31 +0700

    update cv link

--- types/link.type.ts
+++ types/link.type.ts
@@ -2,5 +2,5 @@ export enum Links {
   GITHUB = "https://github.com/femmytedrey",
   LINKEDIN = "https://www.linkedin.com/in/ayomide-adeyemo-61865526b/",
   TWITTER = "https://x.com/femidevs",
-  CV = "https://firebasestorage.googleapis.com/v0/b/my-portfolio-65091.appspot.com/o/Adeyemo_Ayomide%20Full%20Stack%20CV.pdf?alt=media&token=26453b6e-a7c5-473f-8c72-f4db867de121",
+  CV = "https://firebasestorage.googleapis.com/v0/b/my-portfolio-65091.appspot.com/o/Adeyemo%20Ayomide%20Full%20Stack%20CV%20_latest.pdf?alt=media&token=2def5777-957f-4321-94ea-72f14976623a",
 }
